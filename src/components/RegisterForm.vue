f56b437f6af895d1a84fbd23161bd4fa6ef01ff6
mbrignone <mnbrignone@gmail.com>
2023-03-01 10:49:04 -0300

    Update login and register form with the right fields

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -1,66 +1,70 @@
 <template>
   <!-- Modal content -->
-  <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+  <div class="relative bg-white rounded-lg shadow">
     <div class="px-6 py-6 lg:px-8">
-      <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">
-        Sign in to our platform
-      </h3>
+      <h3 class="mb-4 text-xl font-medium text-gray-900">Sign in to our platform</h3>
       <form class="space-y-6" action="#">
+        <!-- Mail -->
         <div>
-          <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >Your email</label
-          >
+          <label for="email" class="block mb-2 text-sm font-bold text-gray-900"> Email </label>
           <input
             type="email"
             name="email"
             id="email"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             placeholder="name@company.com"
             required
           />
         </div>
+        <!-- Password -->
         <div>
-          <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >Your password</label
+          <label for="password" class="block mb-2 text-sm font-bold text-gray-900">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="••••••••"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            required
+          />
+        </div>
+        <!-- Confirm Password -->
+        <div>
+          <label for="password" class="block mb-2 text-sm font-bold text-gray-900"
+            >Confirm Password</label
           >
           <input
             type="password"
             name="password"
             id="password"
             placeholder="••••••••"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             required
           />
         </div>
-        <div class="flex justify-between">
-          <div class="flex items-start">
-            <div class="flex items-center h-5">
-              <input
-                id="remember"
-                type="checkbox"
-                value=""
-                class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-                required
-              />
-            </div>
-            <label for="remember" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-              >Remember me</label
-            >
+        <!-- TOS -->
+        <div class="flex items-start mb-6">
+          <div class="flex items-center h-5">
+            <input
+              id="terms"
+              type="checkbox"
+              value=""
+              class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300"
+              required
+            />
           </div>
-          <a href="#" class="text-sm text-blue-700 hover:underline dark:text-blue-500"
-            >Lost Password?</a
-          >
+          <label for="terms" class="ml-2 text-sm font-medium text-gray-900"
+            >I agree with the
+            <a href="#" class="text-blue-600 hover:underline"> terms and conditions </a>
+          </label>
         </div>
+        <!-- Submit -->
         <button
           type="submit"
-          class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
         >
-          Login to your account
+          Create account
         </button>
-        <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
-          Not registered?
-          <a href="#" class="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
-        </div>
       </form>
     </div>
   </div>
