b02e34dfb325c4123debf3fc10a3bbbc657a3ede
mbrignone <mnbrignone@gmail.com>
2023-03-03 17:32:33 -0300

    Add home and tasks routes

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,10 @@
 <template>
   <nav-bar></nav-bar>
   <div class="min-h-screen bg-gray-100 pt-28 pb-5">
-    <home-view v-if="!userLoggedIn"></home-view>
-    <task-view v-else></task-view>
+    <router-view></router-view>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
-
 import NavBar from "@/components/NavBar.vue";
-import HomeView from "@/views/HomeView.vue";
-import TaskView from "@/views/TaskView.vue";
-
-import useUserStore from "@/stores/userStore";
-
-const userStore = useUserStore();
-const userLoggedIn = ref(userStore.userLoggedIn);
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,52 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-const routes = [];
+import useUserStore from "@/stores/userStore";
+
+const Home = () => import("@/views/HomeView.vue");
+const Tasks = () => import("@/views/TaskView.vue");
+
+const routes = [
+  {
+    name: "home",
+    path: "/",
+    component: Home
+  },
+  {
+    name: "tasks",
+    path: "/tasks",
+    component: Tasks,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: "/:catchAll(.*)*",
+    redirect: { name: "home" }
+  }
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes
+});
+
+router.beforeEach((to, from) => {
+  if (to.meta.requiresAuth) {
+    // redirect to home page if trying to access
+    // a page that requires being auth without
+    // being authenticated
+    const userStore = useUserStore();
+    if (!userStore.userLoggedIn) {
+      return { name: "home" };
+    }
+  } else if (to.name === "home") {
+    // redirect to tasks page if trying to access
+    // home page while being authenticated
+    const userStore = useUserStore();
+    if (userStore.userLoggedIn) {
+      return { name: "tasks" };
+    }
+  }
 });
 
 export default router;
