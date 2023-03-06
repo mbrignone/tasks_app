3b9f602e9a060753a3b8dc249f65e293f03b5f88
mbrignone <mnbrignone@gmail.com>
2023-03-06 12:19:29 -0300

    Make the corresponding API calls to create a new task

--- src/components/AlertForm.vue
+++ src/components/AlertForm.vue
@@ -1,12 +1,10 @@
 <template>
   <div
     v-if="alertInfo.show"
-    class="flex items-center w-full p-4 mb-4 text-gray-500 bg-white rounded-lg shadow"
+    class="flex items-center w-full p-4 mb-4 text-gray-500 rounded-lg shadow"
+    :class="alertInfo.iconBg"
   >
-    <div
-      class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 rounded-lg bg-green-100"
-      :class="alertInfo.iconBg"
-    >
+    <div class="inline-flex items-center justify-center flex-shrink-0 w-6 h-6 rounded-lg">
       <font-awesome-icon :icon="alertInfo.icon" class="w-5 h-5" :class="alertInfo.color" />
     </div>
     <div class="ml-3 text-base font-bold" :class="alertInfo.color">
@@ -15,7 +13,7 @@
     <button
       @click="closeAlert"
       type="button"
-      class="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8"
+      class="ml-auto -mx-1.5 -my-1.5 text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 inline-flex h-8 w-8"
     >
       <font-awesome-icon icon="fa-solid fa-xmark" class="w-5 h-5" />
     </button>
--- src/components/NewTask.vue
+++ src/components/NewTask.vue
@@ -2,13 +2,15 @@
   <div class="flex flex-col items-center">
     <button
       type="button"
-      @click="showNewTask = !showNewTask"
+      @click="closeNewTask"
       class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm sm:w-auto px-3 py-2 text-center mb-5"
     >
       CREATE NEW TASK
       <font-awesome-icon v-if="!showNewTask" icon="fa-solid fa-caret-down" class="ml-2 scale-125" />
       <font-awesome-icon v-else icon="fa-solid fa-caret-up" class="ml-2 scale-125" />
     </button>
+
+    <alert-form :alertInfo="alertInfo" :closeAlert="closeAlert"></alert-form>
     <vee-form v-if="showNewTask" :validation-schema="schema" @submit="createTask" class="w-full">
       <div class="mb-2">
         <label class="block mb-1 text-lg font-bold text-gray-900">Title</label>
@@ -40,7 +42,10 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, reactive } from "vue";
+import { backendPost } from "@/utils/backend_api";
+
+import AlertForm from "@/components/AlertForm.vue";
 
 const showNewTask = ref(false);
 
@@ -51,9 +56,42 @@ const schema = {
 const props = defineProps(["pushTask"]);
 // eslint-disable-next-line vue/no-setup-props-destructure
 const pushTask = props.pushTask;
-function createTask(values) {
-  console.log(values);
+
+const alertInfo = reactive({
+  show: false,
+  message: "",
+  color: "text-green-600",
+  icon: "fa-solid fa-check",
+  iconBg: "bg-green-100"
+});
+async function createTask(values) {
+  try {
+    await backendPost("/api/todos", values);
+  } catch (error) {
+    alertInfo.show = true;
+    alertInfo.message = "Error creating task";
+    alertInfo.color = "text-red-600";
+    alertInfo.iconBg = "bg-red-100";
+    alertInfo.icon = "fa-solid fa-xmark";
+    return;
+  }
+
   pushTask(values);
   showNewTask.value = false;
+
+  alertInfo.message = "Task created!";
+  alertInfo.color = "text-green-600";
+  alertInfo.iconBg = "bg-green-100";
+  alertInfo.icon = "fa-solid fa-check";
+  alertInfo.show = true;
+}
+
+function closeAlert() {
+  alertInfo.show = false;
+}
+
+function closeNewTask() {
+  showNewTask.value = !showNewTask.value;
+  closeAlert();
 }
 </script>
