2294f352f0f5df5b76127bc1bdddb79320d7d689
mbrignone <mnbrignone@gmail.com>
2023-03-05 21:05:54 -0300

    Add support to call backend APIs using Axios in login form

--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -1,15 +1,46 @@
 import { defineStore } from "pinia";
 import { useLocalStorage } from "@vueuse/core";
 
+import { backendGet, backendPost } from "@/utils/backend_api";
+
 export default defineStore("user", {
   state: () => ({
     userInfo: useLocalStorage("userInfo", {})
   }),
 
   actions: {
-    authenticate(email, password) {
-      console.log(email, password);
-      this.userInfo = {};
+    async authenticate(email, password) {
+      const loginData = { username: email, password };
+      let response;
+      try {
+        response = await backendPost("/api/token", loginData, false, true);
+      } catch (error) {
+        if (error.response.status === 400) {
+          return "Invalid email or password";
+        } else {
+          return "Error, please try again late";
+        }
+      }
+
+      const userInfo = {
+        token: response.data.access_token
+      };
+      this.userInfo = useLocalStorage("userInfo", userInfo);
+
+      let userData;
+      try {
+        userData = await backendGet("/api/users/me");
+      } catch (error) {
+        console.log(error);
+      }
+
+      if (userData.data.full_name) {
+        userInfo["name"] = userData.data.full_name;
+      } else {
+        userInfo["name"] = email;
+      }
+
+      this.userInfo = useLocalStorage("userInfo", userInfo, { mergeDefaults: true });
     },
 
     signOut() {
