f3a25dfb42ac546f2183d9baf516242f669b6145
mbrignone <mnbrignone@gmail.com>
2023-03-12 21:04:28 -0300

    Remove Flowbite JS dependencies from dropdown menu

--- src/components/NavBarAvatar.vue
+++ src/components/NavBarAvatar.vue
@@ -1,8 +1,7 @@
 <template>
   <button
-    id="dropdownAvatarNameButton"
-    data-dropdown-toggle="dropdownAvatarName"
-    class="flex items-center text-sm font-medium text-gray-900 rounded-full hover:text-blue-800 md:mr-0 focus:ring-4 focus:ring-gray-100"
+    @click="showDropdown = !showDropdown"
+    class="flex items-center mr-5 text-sm font-medium text-gray-900 rounded-full hover:text-blue-800 md:mr-0 focus:ring-4 focus:ring-gray-100"
     type="button"
   >
     <font-awesome-icon icon="fa-solid fa-user-tie" class="w-6 h-6 rounded-full" />
@@ -12,8 +11,8 @@
 
   <!-- Dropdown menu -->
   <div
-    id="dropdownAvatarName"
-    class="z-10 hidden bg-white divide-y divide-gray-200 rounded-lg shadow w-auto md:w-44"
+    v-if="showDropdown"
+    class="absolute top-12 z-10 bg-white divide-y divide-gray-200 rounded-lg shadow w-auto md:w-44"
   >
     <ul class="py-2 text-sm text-gray-700">
       <li>
@@ -32,15 +31,11 @@
 </template>
 
 <script setup>
-import { onUpdated } from "vue";
-import { initDropdowns } from "flowbite";
+import { ref } from "vue";
 
 import useUserStore from "@/stores/userStore";
 
-// initialize Flowbite dropdown component
-onUpdated(() => {
-  initDropdowns();
-});
+const showDropdown = ref(false);
 
 const userStore = useUserStore();
 function signOut() {
